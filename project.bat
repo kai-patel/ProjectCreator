05ba804823192d5e536940eada17869212804235
Kai Patel <00kaipatel@gmail.com>
2020-09-01 13:33:44 +0100

    Fixed README creation bug

--- project.bat
+++ project.bat
@@ -2,7 +2,7 @@ cd <your-projects-folder>
 if "%~1" == "" GOTO end
 mkdir %1
 cd %1
-echo (# %1) > README.md
+(echo # %1) > README.md
 git init && git add .
 git commit -m "Initial commit"
 python %CD%\main.py %1 %2 %3
