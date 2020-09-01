545e707aa123e8b9a322c1640c9a36264107b11d
Kai Patel <00kaipatel@gmail.com>
2020-09-01 13:37:08 +0100

    Fixed bug where python script would not be called

--- project.bat
+++ project.bat
@@ -5,7 +5,7 @@ cd %1
 (echo # %1) > README.md
 git init && git add .
 git commit -m "Initial commit"
-python %CD%\main.py %1 %2 %3
+python %~dp0\main.py %1 %2 %3
 git remote add origin "<your-account-link>/%1.git"
 git branch -M master
 git push -u origin master
